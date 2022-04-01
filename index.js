0ec36bf6fc2eccc7362347f289aac1f3c193247d
ferparbel <ferparbel@alum.us.es>
2022-04-01 10:58:48 +0200

    resolución problemas

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-const cool = require("cool-ascii-faces");
 const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use("/", express.static('public'));
-
-app.get("/caritas", (Req,res)=>{
-    console.log("Requested /faces route");
-    res.send("<html><body><h1>"+cool()+"</h1></body></html>");
-});
-
-app.get("/time", (Req,res)=>{
-    console.log("Requested /time route");
-    res.send("<html><body><h1>"+new Date()+"</h1></body></html>");
+app.get("/api", (req,res)=>{
+    res.send({ result: true });
 });
 
 app.listen(port, () => {
     console.log(`Server ready at port ${port}`);
 });
-
